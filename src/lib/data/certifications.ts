e02b4f1fdeb93f808ab70e3232f9716fef9e7a3f
jaydev0220 <contact@mengche.dev>
2026-05-21 18:35:21 +0800

    refactor(icons): change certification icons path

--- src/lib/data/certifications.ts
+++ src/lib/data/certifications.ts
@@ -4,16 +4,16 @@ export const certifications: Certification[] = [
 	{
 		badge: 'MCF',
 		fullName: 'My Crypto Fund',
-		iconUrl: '/images/certifications/mcf.svg'
+		iconUrl: '/icons/mcf.svg'
 	},
 	{
 		badge: 'Topstep',
 		fullName: 'Topstep',
-		iconUrl: '/images/certifications/topstep.svg'
+		iconUrl: '/icons/topstep.svg'
 	},
 	{
 		badge: 'FTMO',
 		fullName: 'FTMO',
-		iconUrl: '/images/certifications/ftmo.svg'
+		iconUrl: '/icons/ftmo.svg'
 	}
 ];
